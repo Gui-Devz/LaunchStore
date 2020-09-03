18cc2e92a93cb5672f3cc8bd8e3103bb9fcb8c8d
Guilherme <guilherme_devz@hotmail.com>
2020-09-03 20:29:34 -0300

    Initiating the project

---
+++ src/lib/utils.js
@@ -0,0 +1,45 @@
+module.exports = {
+  formatBrowser(timestamp) {
+    const date = new Date(timestamp);
+    const day = `0${date.getUTCDate()}`.slice(-2);
+    const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+    const year = `${date.getUTCFullYear()}`;
+
+    return {
+      day: day,
+      month: month,
+      year: year,
+      iso: `${year}-${month}-${day}`,
+      birthday: `${day}/${month}`,
+      format: `${day}/${month}/${year}`,
+    };
+  },
+
+  //Function guarantees that the arrays are set for the database
+  arrayDB(array) {
+    let newArray = [];
+
+    for (let i of array) {
+      i = `"${i}"`;
+      newArray.push(i);
+    }
+
+    return `{${newArray}}`;
+  },
+
+  /* Function guarantees that inputs that are arrays don't have blank content
+  inside it like ""
+  */
+  validationOfInputs(inputs) {
+    let newInputs = [];
+    for (let i = 0; i < inputs.length; i++) {
+      const inputClone = inputs[i].trim();
+
+      if (inputClone != "") {
+        newInputs.push(inputs[i]);
+      }
+    }
+
+    return newInputs;
+  },
+};
