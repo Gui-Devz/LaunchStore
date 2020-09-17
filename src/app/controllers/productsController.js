68be1647816cc8d8b9fd28402e6c5e50b997d567
Guilherme <guilherme_devz@hotmail.com>
2020-09-17 18:39:19 -0300

    FIX: database was crashing for some misterious reason so I deleted it and created it again

--- src/app/controllers/productsController.js
+++ src/app/controllers/productsController.js
@@ -7,21 +7,15 @@ module.exports = {
 
   async create(req, res) {
     //pegar categorias
-    /* Category.all()
+    Category.all()
       .then(function (results) {
         const categories = results.rows;
-        console.log(categories);
+
         return res.render("products/create", { categories });
       })
       .catch(function (err) {
         throw new Error(err);
-      }); */
-
-    const results = await Category.all();
-
-    console.log(results.rows);
-
-    return res.render("products/create", { categories: results.rows });
+      });
   },
 
   post(req, res) {},
