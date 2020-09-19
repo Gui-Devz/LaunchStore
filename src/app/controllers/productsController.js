46fcbcf76faeffe443b1340a0ba66d0ab33c523e
Guilherme <guilherme_devz@hotmail.com>
2020-09-18 21:17:06 -0300

    FEATURE: creating the controller for editing the product

--- src/app/controllers/productsController.js
+++ src/app/controllers/productsController.js
@@ -38,6 +38,21 @@ module.exports = {
     results = await Category.all();
     const categories = results.rows;
 
-    return res.render("products/create.njk", { productId, categories });
+    return res.redirect(`/products/${productId}`);
+  },
+
+  async edit(req, res) {
+    let results = await Product.show(req.params.id);
+
+    const product = results.rows[0];
+
+    if (!product) return res.send("product not found!");
+
+    results = await Category.all();
+    const categories = results.rows;
+
+    console.log(product);
+
+    return res.render("products/edit.njk", { product, categories });
   },
 };
