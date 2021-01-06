829be9469164d8f6c48668f737d68e6b2614cfbc
Guilherme <guilherme_devz@hotmail.com>
2021-01-05 22:40:33 -0300

    FIX: Removing the conversion UTC for the formatBrownser function return

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,9 +1,9 @@
 module.exports = {
   formatBrowser(timestamp) {
     const date = new Date(timestamp);
-    const day = `0${date.getUTCDate()}`.slice(-2);
-    const month = `0${date.getUTCMonth() + 1}`.slice(-2);
-    const year = `${date.getUTCFullYear()}`;
+    const day = `0${date.getDate()}`.slice(-2);
+    const month = `0${date.getMonth() + 1}`.slice(-2);
+    const year = `${date.getFullYear()}`;
     const hours = date.getHours();
     const minutes = date.getMinutes();
 
